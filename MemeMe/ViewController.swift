fcea645cf0fabff9122569baf5e738f417b14e5b
Yang Ji <summer@Yangs-MacBook.local>
2016-05-20 15:12:48 -0700

    Pick Image Feature and show on ImageView

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var pickedImageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //check if camera device availabe
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//MARK: Select Image
+    
+    //pick image from camera button or album button
+    @IBAction func pickImage(sender: UIBarButtonItem) {
+        let imageVC = UIImagePickerController()
+        imageVC.delegate = self
+        if sender == cameraButton {
+            imageVC.sourceType = .Camera
+        } else {
+            imageVC.sourceType = .PhotoLibrary
+        }
+        self.presentViewController(imageVC, animated: true, completion: nil)
     }
+  
+    //show image on ImageView
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] {
+            pickedImageView.image = image as? UIImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 
 
 }
