55ec945c593d381535d29b0e69889e98baee3a9c
Yang Ji <summer@Yangs-MacBook.local>
2016-05-20 16:13:42 -0700

    Set up TextField attribute and configuration

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var pickedImageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    var selectedTextField: UITextField!
+    
+//MARK: Text Field Attributes
+    let memeTextFieldAttributes: [String: AnyObject] = [
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: CGFloat(4.0)
+        
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        topTextField.delegate = self
+        topTextField.defaultTextAttributes = memeTextFieldAttributes
+        topTextField.textAlignment = .Center
+        bottomTextField.delegate = self
+        bottomTextField.defaultTextAttributes = memeTextFieldAttributes
+        bottomTextField.textAlignment = .Center
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
         //check if camera device availabe
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+    }
+    
+//MARK: Text Field configure
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        textField.text = ""
+        selectedTextField = textField
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    //dismiss keyboard by touch any other area
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
     }
 
 //MARK: Select Image
